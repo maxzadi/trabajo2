1bc08ea1ff24e876595bf95f49516ee600c67817
maxzadi <maxzadi20@gmail.com>
2025-08-21 20:47:33 -0600

    inicio de UI

--- Controlador.java
+++ Controlador.java
@@ -1,5 +1,28 @@
+import java.util.Date;
+
 import Modelo.ComplejoDeportivo;
 
 public class Controlador {
+    public ComplejoDeportivo complejo;
+
+    public Controlador(){
+        complejo = new ComplejoDeportivo();
+    }
+
+    public void agregarCancha(String tipo, Integer numero, Integer capacidad, Integer costo){
+
+    }
+
+    public void agregarReserva(String responsable, String nombre, String tipo, Date fechaInicio, Date fechaFin){
+
+    }
+
+    public String verCancha(){
+        return null;
+    }
+
+    public String verListaEspera(){
+        return null;
+    }
     
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,39 @@
-import java.util.ResourceBundle.Control;
+import java.util.Date;
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         Controlador controlador= new Controlador();
-        
+        iniciarVentana();
+    }
+
+    public static void iniciarVentana(){
+        JFrame frame =new JFrame("Complejo Deportivo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500,500);
+        frame.add(new JLabel("Complejo Deportivo"));
+
+
+        frame.setVisible(true);
+    }
+
+    public void crearCanchas(){
+
+    }
+
+    public void agregarCancha(){
+
+    }
+
+    public void agregarReserva(){
+
+    }
+
+    public String verCancha(){
+        return null;
+    }
+
+    public String verListaEspera(){
+        return null;
     }
 }
